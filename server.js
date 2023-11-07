2e139329c5d1c1de98758797abb6060fc22d3f59
gimase <kgimase@gmail.com>
2023-11-07 05:25:11 +0300

    port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/requests", requestRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`.yellow.bold);
 });
