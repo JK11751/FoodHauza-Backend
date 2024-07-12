18d78cb2807cdfe59532e99d45227be4eb3ffae6
JK11751 <kgimase@gmail.com>
2024-07-12 22:35:08 +0300

    db updates

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 
 const connectToDB = async () => {
     try {
+        mongoose.set("strictQuery", false);
         const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
