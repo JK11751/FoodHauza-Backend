2c5682057bf037063b52d8253390e8a80b4ddd7c
JK11751 <kgimase@gmail.com>
2024-07-19 15:04:12 +0300

    donation routes updated and corrected

--- routes/donation.routes.js
+++ routes/donation.routes.js
@@ -11,14 +11,12 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(createDonation).get(
-  // protect,
-   allDonations);
+router.route("/").post(createDonation).get(allDonations);
 
-router.route("/:user_id").get(
-  // protect,
-  allUserDonations
-);
+// Adjusted route for user-specific donations
+router.route("/user/:user_id").get(allUserDonations);
+
+// Kept the same for single donation operations
 router.route("/:id").post(updateDonation).get(getDonation).delete(deleteDonation);
 
 module.exports = router;
